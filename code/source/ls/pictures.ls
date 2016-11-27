d6d66a1ce06a5cbfa5cb51ddbe1f3bd5378f9267
刘忍 <18826073587@163.com>
2016-11-27 12:40:22 +0800

    完成个人主页的主体内容

--- code/source/ls/pictures.ls
+++ code/source/ls/pictures.ls
@@ -1,4 +1,7 @@
 angular.module 'pictures', ['ngMaterial']
 
 .controller 'picturesController' !->
-  console.log 'pictures!'
+  # 暂时用代码生成图片的路径
+  @pictures = []
+  for i from 1 to 15
+    @pictures.push '../img/demo/' + i + '.jpg'
--- code/static/js/pictures.js
+++ code/static/js/pictures.js
@@ -1,5 +1,10 @@
 (function(){
   angular.module('pictures', ['ngMaterial']).controller('picturesController', function(){
-    console.log('pictures!');
+    var i$, i;
+    this.pictures = [];
+    for (i$ = 1; i$ <= 15; ++i$) {
+      i = i$;
+      this.pictures.push('../img/demo/' + i + '.jpg');
+    }
   });
 }).call(this);
