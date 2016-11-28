bded10f5f564502ef85315527b9e79dc74c16090
shuq3 <154421897@qq.com>
2016-11-28 22:44:03 +0800

    增加点击效果1.0版

--- code/source/ls/relationship.ls
+++ code/source/ls/relationship.ls
@@ -2,12 +2,16 @@ angular.module 'relationship', ['ngMaterial']
 
 .controller 'relationshipController', ($scope) !->
   text = ["一", "二", "三", "四", "五", "六"]
-
+  $scope.myVar = true;
   $scope.messages = []
   for i from 0 to 5
     $scope.messages.push {
       monthTime: text[i] + "月"
       dayTime: i+1
       share: Math.round Math.random()
-      shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg"]
+      sharenum: 2
+      shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
     }
+
+  $scope.showFriend = !->
+    $scope.myVar = !$scope.myVar;
--- code/static/js/relationship.js
+++ code/static/js/relationship.js
@@ -2,6 +2,7 @@
   angular.module('relationship', ['ngMaterial']).controller('relationshipController', function($scope){
     var text, i$, i;
     text = ["一", "二", "三", "四", "五", "六"];
+    $scope.myVar = true;
     $scope.messages = [];
     for (i$ = 0; i$ <= 5; ++i$) {
       i = i$;
@@ -9,8 +10,12 @@
         monthTime: text[i] + "月",
         dayTime: i + 1,
         share: Math.round(Math.random()),
-        shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg"]
+        sharenum: 2,
+        shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
       });
     }
+    $scope.showFriend = function(){
+      $scope.myVar = !$scope.myVar;
+    };
   });
 }).call(this);
