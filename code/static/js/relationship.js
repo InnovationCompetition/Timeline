26a3fb8044ec3b2438d2541f9a3fe895fe32d003
刘忍 <18826073587@163.com>
2016-11-29 18:01:31 +0800

    解决之前导致的冲突

--- code/static/js/relationship.js
+++ code/static/js/relationship.js
@@ -1,6 +1,5 @@
 (function(){
   angular.module('relationship', ['ngMaterial']).controller('relationshipController', function($scope){
-<<<<<<< HEAD
     var numberOfDays, i$, month, j$, to$, date, shareFaces, k$, i;
     $scope.monthText = ["一月", "二月", "三月"];
     numberOfDays = [31, 28, 31];
@@ -32,25 +31,6 @@
         message = ref$[i$];
         message.isOpen = false;
       }
-=======
-    var text, i$, i;
-    text = ["一", "二", "三", "四", "五", "六"];
-    $scope.hide = true;
-    $scope.messages = [];
-    for (i$ = 0; i$ <= 5; ++i$) {
-      i = i$;
-      $scope.messages.push({
-        monthTime: text[i] + "月",
-        dayTime: i + 1,
-        share: Math.round(Math.random()),
-        sharenum: 3,
-        shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
-      });
-    }
-    $scope.showFriend = function(){
-      $scope.hide = !$scope.hide;
-      console.log($scope.hide);
->>>>>>> origin/master
     };
   });
 }).call(this);
