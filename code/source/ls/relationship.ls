622b770652fa7c6422626b72bb43f50d0aa846f0
shuq3 <154421897@qq.com>
2016-11-29 10:59:39 +0800

    small change

--- code/source/ls/relationship.ls
+++ code/source/ls/relationship.ls
@@ -2,16 +2,22 @@ angular.module 'relationship', ['ngMaterial']
 
 .controller 'relationshipController', ($scope) !->
   text = ["一", "二", "三", "四", "五", "六"]
-  $scope.myVar = true;
+  $scope.hide = true
   $scope.messages = []
   for i from 0 to 5
     $scope.messages.push {
       monthTime: text[i] + "月"
       dayTime: i+1
       share: Math.round Math.random()
-      sharenum: 2
+      sharenum: 3
       shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
     }
 
+  # $scope.showFriend = (e)!->
+  #   a = e.target.parentNode
+  #   # a.hide = !a.hide
+  #   console.log a
+
   $scope.showFriend = !->
-    $scope.myVar = !$scope.myVar;
+    $scope.hide = !$scope.hide
+    console.log $scope.hide
--- code/static/js/relationship.js
+++ code/static/js/relationship.js
@@ -2,7 +2,7 @@
   angular.module('relationship', ['ngMaterial']).controller('relationshipController', function($scope){
     var text, i$, i;
     text = ["一", "二", "三", "四", "五", "六"];
-    $scope.myVar = true;
+    $scope.hide = true;
     $scope.messages = [];
     for (i$ = 0; i$ <= 5; ++i$) {
       i = i$;
@@ -10,12 +10,13 @@
         monthTime: text[i] + "月",
         dayTime: i + 1,
         share: Math.round(Math.random()),
-        sharenum: 2,
+        sharenum: 3,
         shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
       });
     }
     $scope.showFriend = function(){
-      $scope.myVar = !$scope.myVar;
+      $scope.hide = !$scope.hide;
+      console.log($scope.hide);
     };
   });
 }).call(this);
