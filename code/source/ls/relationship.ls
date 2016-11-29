fd1fedb1098750e81f5ba03d3ee26a02b570e1bf
刘忍 <18826073587@163.com>
2016-11-29 17:54:54 +0800

    更改页面顺序，修改关系页面

--- code/source/ls/relationship.ls
+++ code/source/ls/relationship.ls
@@ -1,17 +1,24 @@
 angular.module 'relationship', ['ngMaterial']
 
 .controller 'relationshipController', ($scope) !->
-  text = ["一", "二", "三", "四", "五", "六"]
-  $scope.myVar = true;
+  $scope.monthText = ["一月", "二月", "三月"]
+  numberOfDays = [31, 28, 31]
   $scope.messages = []
-  for i from 0 to 5
-    $scope.messages.push {
-      monthTime: text[i] + "月"
-      dayTime: i+1
-      share: Math.round Math.random()
-      sharenum: 2
-      shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
-    }
+  for month from 0 to 2
+    for date from 1 to numberOfDays[month]
+      shareFaces = []
+      for i from 1 to 5
+        if parseInt(4 * Math.random()) == 0
+          shareFaces.push("../img/demo/" + i + ".jpg")
+      $scope.messages.push {
+        month: $scope.monthText[month]
+        date: date
+        isShare: parseInt(5 * Math.random()) == 0
+        shareNum: shareFaces.length
+        shareFaces: shareFaces
+        isOpen: false
+      }
 
   $scope.showFriend = !->
-    $scope.myVar = !$scope.myVar;
+    for message in $scope.messages
+      message.isOpen = false
--- code/static/js/relationship.js
+++ code/static/js/relationship.js
@@ -1,21 +1,36 @@
 (function(){
   angular.module('relationship', ['ngMaterial']).controller('relationshipController', function($scope){
-    var text, i$, i;
-    text = ["一", "二", "三", "四", "五", "六"];
-    $scope.myVar = true;
+    var numberOfDays, i$, month, j$, to$, date, shareFaces, k$, i;
+    $scope.monthText = ["一月", "二月", "三月"];
+    numberOfDays = [31, 28, 31];
     $scope.messages = [];
-    for (i$ = 0; i$ <= 5; ++i$) {
-      i = i$;
-      $scope.messages.push({
-        monthTime: text[i] + "月",
-        dayTime: i + 1,
-        share: Math.round(Math.random()),
-        sharenum: 2,
-        shareFace: ["../img/demo/1.jpg", "../img/demo/2.jpg", "../img/demo/3.jpg"]
-      });
+    for (i$ = 0; i$ <= 2; ++i$) {
+      month = i$;
+      for (j$ = 1, to$ = numberOfDays[month]; j$ <= to$; ++j$) {
+        date = j$;
+        shareFaces = [];
+        for (k$ = 1; k$ <= 5; ++k$) {
+          i = k$;
+          if (parseInt(4 * Math.random()) === 0) {
+            shareFaces.push("../img/demo/" + i + ".jpg");
+          }
+        }
+        $scope.messages.push({
+          month: $scope.monthText[month],
+          date: date,
+          isShare: parseInt(5 * Math.random()) === 0,
+          shareNum: shareFaces.length,
+          shareFaces: shareFaces,
+          isOpen: false
+        });
+      }
     }
     $scope.showFriend = function(){
-      $scope.myVar = !$scope.myVar;
+      var i$, ref$, len$, message;
+      for (i$ = 0, len$ = (ref$ = $scope.messages).length; i$ < len$; ++i$) {
+        message = ref$[i$];
+        message.isOpen = false;
+      }
     };
   });
 }).call(this);
