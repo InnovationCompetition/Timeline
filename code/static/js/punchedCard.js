dde65504f9911d79fc73b3a18e37010ae58f756f
刘忍 <18826073587@163.com>
2016-11-30 13:02:13 +0800

    编译佳豪写的代码

--- code/static/js/punchedCard.js
+++ code/static/js/punchedCard.js
@@ -1,3 +1,11 @@
 (function(){
-  angular.module('punchedCard', ['ngMaterial']).controller('punchedCardController', function(){});
+  angular.module('punchedCard', ['ngMaterial', 'ngMessages']).controller('punchedCardController', function($scope){
+    var this$ = this;
+    $scope.myDate = new Date();
+    $scope.minDate = new Date($scope.myDate.getFullYear(), $scope.myDate.getMonth() - 4, $scope.myDate.getDate());
+    $scope.maxDate = new Date();
+    $scope.theDate = function(date){
+      return date.toUTCString() === new Date(2016, 10, 11).toUTCString() || date.toUTCString() === new Date(2016, 10, 15).toUTCString() || date.toUTCString() === new Date(2016, 10, 27).toUTCString();
+    };
+  });
 }).call(this);
