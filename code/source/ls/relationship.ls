50f1bd7fa423a07f2a6556ed7d568621d26e0121
shuq3 <154421897@qq.com>
2016-12-01 01:28:23 +0800

    获取点击位置信息

--- code/source/ls/relationship.ls
+++ code/source/ls/relationship.ls
@@ -3,7 +3,7 @@ angular.module 'relationship', ['ngMaterial']
 .controller 'relationshipController', ($scope, $interval) !->
   # 控制数据绑定
   $scope.monthText = ["一月", "二月", "三月"]
-  numberOfDays = [31, 28, 31]
+  numberOfDays = [10, 10, 10]
   $scope.messages = []
   for month from 0 to 2
     for date from 1 to numberOfDays[month]
@@ -30,7 +30,49 @@ angular.module 'relationship', ['ngMaterial']
       $('canvas').width = $('list').offsetWidth
   , 500
 
+  # getElementLeft = (element) ->
+  #   actualLeft = element.offsetLeft
+  #   current = element.offsetParent
+  #   loop
+  #     actualLeft = actualLeft + current.offsetLeft
+  #     console.log current
+  #     current = current.offsetParent
+  #     break if  current != null
+  #   actualLeft
+  #
+  # getElementTop = (element) ->
+  #   actualTop  = element.offsetTop
+  #   current = element.offsetParent
+  #   loop
+  #     actualTop = actualTop + current.offsetTop
+  #     current = current.offsetParent
+  #     break if  current != null
+  #   actualTop
+
   # 控制好友头像的展开
   $scope.showFriend = !->
     for message in $scope.messages
       message.isOpen = false
+
+  $scope.getFriend = (e, message, image) !->
+    message.isOpen = true
+    x = e.target.getBoundingClientRect().left+document.documentElement.scrollLeft
+    y =  e.target.getBoundingClientRect().top+document.documentElement.scrollTop
+    console.log x, y
+    ctx = $('canvas').getContext("2d")
+    ctx.beginPath()
+    ctx.lineWidth=5
+    ctx.lineJoin="round"
+    ctx.moveTo(x,y)
+    for mess in document.getElementsByTagName("md-list-item")
+      img = mess.getElementsByTagName("img")
+      for search in img
+        str1 = String(search.src)
+        str1 = str1.substr(str1.length-5, 5)
+        str2 = image.substr(image.length-5, 5)
+        if str1 == str2
+          point_x = mess.getBoundingClientRect().left+document.documentElement.scrollLeft
+          point_y = mess.getBoundingClientRect().top+document.documentElement.scrollTop
+          ctx.lineTo(point_x,point_y)
+          console.log point_x, point_y
+    ctx.stroke()
--- code/static/js/relationship.js
+++ code/static/js/relationship.js
@@ -1,8 +1,8 @@
 (function(){
   angular.module('relationship', ['ngMaterial']).controller('relationshipController', function($scope, $interval){
-    var numberOfDays, i$, month, j$, to$, date, shareFaces, k$, i, $, this$ = this;
+    var numberOfDays, i$, month, j$, to$, date, shareFaces, k$, i, $, getElementLeft, getElementTop, this$ = this;
     $scope.monthText = ["一月", "二月", "三月"];
-    numberOfDays = [31, 28, 31];
+    numberOfDays = [10, 10, 10];
     $scope.messages = [];
     for (i$ = 0; i$ <= 2; ++i$) {
       month = i$;
@@ -34,12 +34,68 @@
         $('canvas').width = $('list').offsetWidth;
       }
     }, 500);
+    getElementLeft = function(element){
+      var actualLeft, current;
+      actualLeft = element.offsetLeft;
+      current = element.offsetParent;
+      for (;;) {
+        actualLeft = actualLeft + current.offsetLeft;
+        console.log(current);
+        current = current.offsetParent;
+        if (current !== null) {
+          break;
+        }
+      }
+      return actualLeft;
+    };
+    getElementTop = function(element){
+      var actualTop, current;
+      actualTop = element.offsetTop;
+      current = element.offsetParent;
+      for (;;) {
+        actualTop = actualTop + current.offsetTop;
+        current = current.offsetParent;
+        if (current !== null) {
+          break;
+        }
+      }
+      return actualTop;
+    };
     $scope.showFriend = function(){
       var i$, ref$, len$, message;
       for (i$ = 0, len$ = (ref$ = $scope.messages).length; i$ < len$; ++i$) {
         message = ref$[i$];
         message.isOpen = false;
       }
+    };
+    $scope.getFriend = function(e, message, image){
+      var x, y, ctx, i$, ref$, len$, mess, img, j$, len1$, search, str1, str2, point_x, point_y;
+      message.isOpen = true;
+      x = e.target.getBoundingClientRect().left + document.documentElement.scrollLeft;
+      y = e.target.getBoundingClientRect().top + document.documentElement.scrollTop;
+      console.log(x, y);
+      ctx = $('canvas').getContext("2d");
+      ctx.beginPath();
+      ctx.lineWidth = 5;
+      ctx.lineJoin = "round";
+      ctx.moveTo(x, y);
+      for (i$ = 0, len$ = (ref$ = document.getElementsByTagName("md-list-item")).length; i$ < len$; ++i$) {
+        mess = ref$[i$];
+        img = mess.getElementsByTagName("img");
+        for (j$ = 0, len1$ = img.length; j$ < len1$; ++j$) {
+          search = img[j$];
+          str1 = String(search.src);
+          str1 = str1.substr(str1.length - 5, 5);
+          str2 = image.substr(image.length - 5, 5);
+          if (str1 === str2) {
+            point_x = mess.getBoundingClientRect().left + document.documentElement.scrollLeft;
+            point_y = mess.getBoundingClientRect().top + document.documentElement.scrollTop;
+            ctx.lineTo(point_x, point_y);
+            console.log(point_x, point_y);
+          }
+        }
+      }
+      ctx.stroke();
     };
   });
 }).call(this);
