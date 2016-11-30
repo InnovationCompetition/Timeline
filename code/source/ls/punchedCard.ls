2a8deb27791609af977b1f617ede1d88339f1640
8MarkOut <wudixjh@sina.com>
2016-11-30 14:23:01 +0800

    完成punchcard的主体功能

--- code/source/ls/punchedCard.ls
+++ code/source/ls/punchedCard.ls
@@ -17,5 +17,10 @@ angular.module 'punchedCard', ['ngMaterial', 'ngMessages']
   {
     link: (scope, element, attrs) !->
       element.ready !->
-        document.getElementsByClassName('md-calendar-scroll-mask')[0].style.height = (document.body.clientHeight - 480)  + 'px'
+        document.getElementsByClassName('md-calendar-scroll-mask')[0].style.height = (window.screen.height - 370)  + 'px'
+        date = new Array!
+        date[0] = "2016-10-11"
+        date[1] = "2016-10-15"
+        for i from 0 to date.length - 1
+           document.getElementById('md-0-month-'+date[i]).childNodes[0].style.background = 'rgb(33,150,243)'
   }
--- code/static/js/punchedCard.js
+++ code/static/js/punchedCard.js
@@ -12,7 +12,15 @@
     return {
       link: function(scope, element, attrs){
         element.ready(function(){
-          document.getElementsByClassName('md-calendar-scroll-mask')[0].style.height = (document.body.clientHeight - 480) + 'px';
+          var date, i$, to$, i;
+          document.getElementsByClassName('md-calendar-scroll-mask')[0].style.height = (window.screen.height - 370) + 'px';
+          date = new Array();
+          date[0] = "2016-10-11";
+          date[1] = "2016-10-15";
+          for (i$ = 0, to$ = date.length - 1; i$ <= to$; ++i$) {
+            i = i$;
+            document.getElementById('md-0-month-' + date[i]).childNodes[0].style.background = 'rgb(33,150,243)';
+          }
         });
       }
     };
